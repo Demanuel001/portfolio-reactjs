bb6ffa3bd51671dbc1c7c94f5a4a89ebea3842f8
D'Sousa <43572938+Demanuel001@users.noreply.github.com>
2023-03-08 16:50:29 -0300

    update information

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -8,10 +8,17 @@ const AboutContainer = () => {
             Olá, eu sou o Danilo. <br />
             Sou uma pessoa comunicativa, organizada e responsável, <br />
             minha paixão por programação veio quando iniciei o curso técnico em Informática em 2015, 
-            onde aprendi e estudei bastante, principalmente o tão famoso Java, no ano seguinte iniciei o 
+            onde aprendi e estudei bastante, principalmente a linguagem Java, no ano seguinte iniciei o 
             curso de Robótica e tive a oportunidade de aprender sobre o Arduino e sua linguagem 
-            bem semelhante à linguagem C, com isso obtive experiências incríveis. <br />
+            bem semelhante à linguagem C, também tive a opotunidade de usar Lego EV3, com isso obtive experiências incríveis. <br />
             Atualmente estou no 7° período do curso de Sistemas de Informação (Estácio de SÁ). <br />
+        </p>
+        <p>
+            Ingressei na área de programação em setembro de 2018 e desde então trabalho com: 
+           -xHarbour no ERP atual da empresa, tabelas DBF;
+           -Desenvolvimento de aplicações, melhoria e manutenção de ERP com xHarbour;
+           -Java EE, Java SPRING, PL/SQL;
+           -SGBD PostgreSQL para criação e manutenção de tabelas.
         </p>
     </section>
   )
